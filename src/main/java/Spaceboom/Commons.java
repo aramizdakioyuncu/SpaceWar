220a6f920fb0cea020702be75ba17b933ac63d16
engin <enginku118@gmail.com>
2023-12-27 03:04:14 +0300

    erken finish düzeldi

--- src/main/java/Spaceboom/Commons.java
+++ src/main/java/Spaceboom/Commons.java
@@ -16,7 +16,7 @@ public class  Commons {
     public static int ALIEN_INIT_Y = 5;
 
     public static int GO_DOWN = 15;
-    public static int NUMBER_OF_ALIENS_TO_DESTROY = 2;
+    public static int NUMBER_OF_ALIENS_TO_DESTROY = 24;
     public static int CHANCE = 5;
     public static int DELAY = 19;
     public static int PLAYER_WIDTH;
